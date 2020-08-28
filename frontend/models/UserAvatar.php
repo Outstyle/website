884fb29199ffcfb1e06a10a9147703b069d51b52
Eugene Esca <scsmash3r@gmail.com>
2020-08-28 15:15:15 +0300

    Temporary avatar image fix (broken)

--- frontend/models/UserAvatar.php
+++ frontend/models/UserAvatar.php
@@ -1,9 +1,11 @@
 <?php
+
 /**
  * @link https://github.com/Outstyle/website
  * @copyright Copyright (c) 2018 Outstyle Network
  * @license Beerware
  */
+
 namespace frontend\models;
 
 use Yii;
@@ -23,38 +25,38 @@ class UserAvatar extends \common\models\Photo
     public function behaviors()
     {
         return [
-        /**
+            /**
          * imageUploaderBehavior - https://github.com/demisang/yii2-image-uploader
          */
-        'imageUploaderBehavior' => [
-          'class' => 'demi\image\ImageUploaderBehavior',
-          'imageConfig' => [
-            'imageAttribute' => 'img',
-            'savePathAlias' => Yii::$app->params['imagesSubdomainDir'].Yii::$app->params['avatarPathUrl'],
-            'rootPathAlias' => Yii::$app->params['imagesSubdomainDir'],
-            'noImageBaseName' => 'noimage.jpg',
-            'imageSizes' => [
-              '' => 2000, /* Also serves as a maxWidth limit for uploaded file and only for this set of image sizes */
-              '150x150_' => 150, /* 1/1 aspect ratio */
+            'imageUploaderBehavior' => [
+                'class' => 'demi\image\ImageUploaderBehavior',
+                'imageConfig' => [
+                    'imageAttribute' => 'img',
+                    'savePathAlias' => Yii::$app->params['imagesSubdomainDir'] . Yii::$app->params['avatarPathUrl'],
+                    'rootPathAlias' => Yii::$app->params['imagesSubdomainDir'],
+                    'noImageBaseName' => 'noimage.jpg',
+                    'imageSizes' => [
+                        '' => 2000, /* Also serves as a maxWidth limit for uploaded file and only for this set of image sizes */
+                        '150x150_' => 150, /* 1/1 aspect ratio */
+                    ],
+                    'imageValidatorParams' => [
+                        'minWidth' => 150,
+                        'minHeight' => 150,
+                        'maxWidth' => 2000,
+                        'maxHeight' => 2000,
+                    ],
+                    'aspectRatio' => [
+                        1 / 1,
+                    ],
+                    'imageRequire' => false,
+                    'uploadMultiple' => 1,
+                    'deleteRow' => false,
+                    'fileTypes' => self::ALLOWED_FILE_TYPES,
+                    'maxFileSize' => 3145728,
+                    'backendSubdomain' => 'admin.',
+                ],
             ],
-            'imageValidatorParams' => [
-              'minWidth' => 150,
-              'minHeight' => 150,
-              'maxWidth' => 2000,
-              'maxHeight' => 2000,
-            ],
-            'aspectRatio' => [
-              1 / 1,
-            ],
-            'imageRequire' => false,
-            'uploadMultiple' => 1,
-            'deleteRow' => false,
-            'fileTypes' => self::ALLOWED_FILE_TYPES,
-            'maxFileSize' => 3145728,
-            'backendSubdomain' => 'admin.',
-          ],
-        ],
-      ];
+        ];
     }
 
 
@@ -67,14 +69,15 @@ class UserAvatar extends \common\models\Photo
      * @param  integer $serviceId
      * @return string
      */
-    public static function getAvatarPath($relativePath = '', $size = '150x150_', $serviceId = 0) : string
+    public static function getAvatarPath($relativePath = '', $size = '150x150_', $serviceId = 0): string
     {
         /* Avatar for deleted or inactive user */
         if (!$relativePath) {
-            return Yii::$app->params['imagesPathUrl'].'images/54x54_avatar_deleted.png';
+            return Yii::$app->params['imagesPathUrl'] . 'images/54x54_avatar_deleted.png';
         }
 
-        return self::getByPrefixAndServiceId($relativePath, $size, $serviceId, $photoType = self::PHOTO_TYPE_AVATAR);
+        //return self::getByPrefixAndServiceId($relativePath, $size, $serviceId, $photoType = self::PHOTO_TYPE_AVATAR);
+        return '/css/i/broken/avatar_128x128.png';
     }
 
     /**
@@ -82,10 +85,10 @@ class UserAvatar extends \common\models\Photo
      * @param  int  $avatarId
      * @return string
      */
-    public static function getById(int $avatarId = 0) : string
+    public static function getById(int $avatarId = 0): string
     {
         if ($avatarId === 0) {
-            return Yii::$app->params['imagesPathUrl'].'images/54x54_avatar_deleted.png';
+            return Yii::$app->params['imagesPathUrl'] . 'images/54x54_avatar_deleted.png';
         }
 
         $photo = self::find()
@@ -93,7 +96,8 @@ class UserAvatar extends \common\models\Photo
             ->asArray()
             ->one();
 
-        return self::getAvatarPath($photo['img'], '150x150_', $photo['service_id']);
+        //return self::getAvatarPath($photo['img'], '150x150_', $photo['service_id']);
+        return '/css/i/broken/avatar_128x128.png';
     }
 
     /**
@@ -101,7 +105,7 @@ class UserAvatar extends \common\models\Photo
      * @param  int  $userId
      * @return string
      */
-    public static function getByUserId(int $userId = 0) : string
+    public static function getByUserId(int $userId = 0): string
     {
         $avatar = UserDescription::find()
             ->select('avatar')
